35e99a447fb0b0d9cc4a3064a4df34d367f232b2
goldendune <thegoldendune@gmail.com>
2021-01-27 18:24:31 -0300

    final exercise

---
+++ js/main.js
@@ -0,0 +1,98 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar.</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por Clicar");
+}
+
+function redirecionar(){
+    window.open("https://globallabs.academy/");
+    //window.location.href = "https://globallabs.academy/";
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+var validar = 0;
+
+function validaIdade(idade){
+    if (idade >= 18){
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("qual sua idade?");
+validaIdade(idade);
+console.log(validar);
+*/
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count=0; count<=5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("Maior de idade.");
+}
+else{
+    alert("Menor de idade.")
+};
+*/
+
+/*
+var frutas = [{nome: "maça", cor: "veremlha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome: "maça", cor: "veremlha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+var lista = ["maça","pêra","laranja"];
+//lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+/*
+var nome = "Abner Aguiar";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo.";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+//alert(frase.replace("Japão","Brasil"));
+*/
